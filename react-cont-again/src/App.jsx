b3439418afdaa791d29668dd9e7d5487e2752763
ashish29-tech <ashishranjan503@gmail.com>
2025-10-04 11:28:15 +0530

    new changes

--- react-cont-again/src/App.jsx
+++ react-cont-again/src/App.jsx
@@ -5,6 +5,7 @@ function App() {
   return (
     <div>
       <h1>hello react</h1>
+      <h1>hello react</h1>
       {/* yha include karenge Person component. And jab kisi component ko call karte hai toh args bhej sakte hai */}
       <Person/>
     </div>
