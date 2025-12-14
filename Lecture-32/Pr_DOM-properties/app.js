6726812eaf78b39f579ba3fa8f44168acbd9b71d
ashish29-tech <ashishranjan503@gmail.com>
2025-12-14 17:17:33 +0530

    DOM propt practice

--- Lecture-32/Pr_DOM-properties/app.js
+++ Lecture-32/Pr_DOM-properties/app.js
@@ -8,6 +8,16 @@ let p = document.querySelector("p");
 //innerText brainful hota hai...this can read css..style diya html m toh use isne padh liya. 
 // console.log(p.innerText)
 
+//Ye textContent bhi getter and setter ka kaam kar rha hai
+//Ye tettContent bhi text de rha hai...textContent brainless hota hai...unaware of the styling
+// console.log(p.textContent)
+
+//Ye innerHTML bhi getter and setter ka kaam kar rha hai
+//innerHTML....this comes with the tag and styling itself. This is also brainful..read css + shows tags bhi
+// console.log(p.innerHTML)
+
+
+
 
 
 
