1234232704858924cea16fabe305460125675f73
ashish29-tech <ashishranjan503@gmail.com>
2025-12-31 16:03:03 +0530

    jQuery Effects

--- Lecture-38/events-jQuery/app.js
+++ Lecture-38/events-jQuery/app.js
@@ -34,12 +34,28 @@
 //---------------------------------------------
 //addEventListener ki jagah on event hai yha
 
-$('button').on('click' , function(){ //button pe click kiya jaye tab 1 function chla do
- //console.log("Hello");
- let ans = $('input').val() //input ko select kiya aur uski value show kar di
- console.log(ans)
- console.log($('input').val())
-})
+// $('button').on('click' , function(){ //button pe click kiya jaye tab 1 function chla do
+//  //console.log("Hello");
+//  let ans = $('input').val() //input ko select kiya aur uski value show kar di
+//  console.log(ans)
+//  console.log($('input').val())
+// })
 
 
+//----------------------------------------------
+//Effects in jQuery
+//inpe on event
+$('#fadeIn').on('click' , function(){ //button ko select kiya...since they are all id's toh hash lag jayega. jab bhi in button pe click ho to function run ho jaye
+  $('#container').fadeIn() //container ko change karna hai to container select kiya...and iske upar properties(fadeIn, fadeOut)
+}) 
+$('#fadeOut').on('click' , function(){
+  $('#container').fadeOut() 
+})  
+$('#fadeToggle').on('click' , function(){
+  $('#container').fadeToggle() 
+}) 
 
+
+//slideIn, slideOut effects...read docs 
+
+//to do wale project jQuery ki help se kar sakte hai
