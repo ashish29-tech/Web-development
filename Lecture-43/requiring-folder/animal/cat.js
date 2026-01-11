7076fb840e7e94872460a70ae5bcf1624dadf132
ashish29-tech <ashishranjan503@gmail.com>
2026-01-11 23:50:19 +0530

    node requiring

--- Lecture-43/requiring-folder/animal/cat.js
+++ Lecture-43/requiring-folder/animal/cat.js
@@ -7,10 +7,10 @@
 // module.exports = { cat:cat } //cat ki jgah cat likha hai
 // module.exports = { cat }
 
-
 const cat = ()=>{
   console.log("meow meow")
 }
+
 
 // module.exports = {cat : cat}
 module.exports = {cat}
