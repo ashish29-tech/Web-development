6fcfe4a5af91b11c36f469d375b77fe98511a123
ashish29-tech <ashishranjan503@gmail.com>
2026-01-23 16:05:52 +0530

    Cookies

--- Lecture-60/cookies/index.js
+++ Lecture-60/cookies/index.js
@@ -4,8 +4,8 @@ const app = express();
 
 const cookieParser = require('cookie-parser'); //cookie parser required..code copied from google...npm cookie parser
 
-//run karne keloye cookie parser ko middleware chahiye hoga
-app.use(cookieParser('hiiamyoursecretteacher')); //secret
+//run karne ke liye cookie parser ko middleware chahiye hoga. This middleware can accept a secret. Secret key is basically an array or string....bhaiya always gives a string.
+app.use(cookieParser('hiiamyoursecretteacher')); //secret......ye signed cookie ke liye. 'hiiamyoursecretteacher'....ye ham kahi aur dal dete and use yha import kara lete....but abhi ke liye dikha lete hai
 
 //jo cookie parser require kiya hai and use run karna hai to as a middleware run karna padega
 // app.use(cookieParser());
@@ -13,11 +13,11 @@ app.use(cookieParser('hiiamyoursecretteacher')); //secret
 
 app.get('/', (req,res)=>{ //ye route bnaya..ye hame sare cookies lake de rha hai
   // console.log(req.cookies); // request m hoti hai cookies
-  // res.send(req.cookies) //and ise send bhi kara lete hai
+  // res.send(req.cookies) //and ise send bhi kara lete hai...and if we see root route pe hit kare toh password nahi dikhega
+  //signed cookies dekhne ke liye...//https://expressjs.com/en/5x/api.html#req.signedCookies  
   console.log(req.signedCookies); //expressjs.com...docs se. signedCookies kiya
   res.send(req.signedCookies); //signedCookies ko send kara diya. Ye pehle se toh comparatively secure hai.
 })
-
 
 
 //cookie server bhejta hai...so we make route jo server send karaye cookies ki to the client. To sabse pehle server send karega...and server response send karta hai
@@ -39,8 +39,10 @@ app.get('/setcookies', (req,res)=>{ //ye /setcookies route hit karenge browser m
 app.get('/getcookies', (req,res)=>{ // now hit the route...http://localhost:5050/getcookies...sari cookies nazar ayengi in thr format of object..toh we can destructure it
   //When using cookie-parser middleware, this property is an object that contains cookies sent by the request. If the request contains no cookies, it defaults to {}.
   //in the format of object mil sakta hai toh ise dekhne ke liye...req.cookies ko access karne ke liye console
-  console.log(req.cookies); //req.cookies from expressjs docs. get karne ke liye...request se mangenge and sari cookies mangenge isliye plural hoga toh s hoga cookies m. and store client side pe hoti hai and client request bhejta hai isliye req.cookies
-  // let {username, age} = req.cookies; //jo bhi chahiye use destructure kar diya...like hame username and age chcahiye
+  // console.log(req.cookies); //req.cookies from expressjs docs. get karne ke liye...request se mangenge and sari cookies mangenge isliye plural hoga toh s hoga cookies m. and store client side pe hoti hai and client request bhejta hai isliye req.cookies
+  //pura object dega isliye req.cookies....
+  //client request bhejta hai...toh use dekhne ke liye req.cookies
+  let {username, age} = req.cookies; //jo bhi chahiye use destructure kar diya...like hame username and age chcahiye
   // res.send(req.cookies)
   res.send(`hi I am ${username} and my age is ${age}`) //destructure kar liya tph we can send something like this
 
@@ -50,3 +52,4 @@ app.get('/getcookies', (req,res)=>{ // now hit the route...http://localhost:5050
 app.listen(5050, ()=>{
   console.log("server at 5050")
 })
+
