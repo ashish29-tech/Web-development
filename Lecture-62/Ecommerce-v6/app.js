837d8dba63f810cadb2526163f532c0be4f012b9
ashish29-tech <ashishranjan503@gmail.com>
2026-01-27 16:55:02 +0530

    Ecommerce v6

---
+++ Lecture-62/Ecommerce-v6/app.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const mongoose = require('mongoose');
+const seedDB = require('./seed');
+const methodOverride  = require('method-override');
+const session = require('express-session');
+const flash = require('connect-flash');
+const productRoutes = require("./routes/productRoutes");
+const reviewRoutes = require("./routes/review");
+const authRoutes = require("./routes/auth"); //auth route bna diya.
+const passport = require('passport'); //passport command
+const LocalStrategy = require('passport-local'); ///passport command
+const User = require('./models/User'); ///passport command...user yha require ho gya..niche user pe click karte hi.
+
+
+mongoose.set('strictQuery', true);
+mongoose.connect('mongodb://127.0.0.1:27017/julybatch')
+.then(()=>{console.log("DB connected")})
+.catch((err)=>{console.log(err)})
+ 
+
+app.set('view engine' , 'ejs');
+app.set('views' , path.join(__dirname,'views'));
+// now for public folder
+app.use(express.static(path.join(__dirname,'public')));
+app.use(express.urlencoded({extended:true}));
+app.use(methodOverride('_method'));
+
+// seeding dummy data
+// seedDB();
+
+let configSession = { //ye session ka middleware hai.
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { //cookie ke andar 1 object bheja
+        httpOnly: true, //abhi secure itna nai rakhna toh isrf http...and sessionId hp hi rahe....and ye seesion id 
+        expires: Date.now() + 7*24*60*60*1000, //abhi time ka pta lag rha hai Date.now() se...7 days * 24 ghante * 60 minute * 60 seconds * 1000 millisecond in 1 minute....itni der baad expire ho jaye
+        maxAge: 7*24*60*60*1000 //yahi iski umr hui...7 days. This is how we manage session ka time. Expire ya maxAge dono m se jo pehle aa jaye..isliye ham dono ko same likhte hai
+    }
+}
+
+app.use(session(configSession)); //session ke baad niche passport 
+app.use(flash());
+
+
+// use static serialize and deserialize of model for passport session support
+app.use(passport.initialize()); //passport initialise and upar require kar liya.
+app.use(passport.session()); //pass. session is a different package and passport is a different package...so we want our passport to get the access of session....passport ab mere session ka bhi use kar sakta hai.
+passport.serializeUser(User.serializeUser()); //passport command...ye passport ke andar serialise iske liye passport ke session ko 
+passport.deserializeUser(User.deserializeUser()); //passport command
+
+// use static authenticate method of model in LocalStrategy
+passport.use(new LocalStrategy(User.authenticate())); //jha serialise kiya hai user ko wha user bhi to chahiye hoga...upar require kar liya
+
+
+app.use((req,res,next)=>{
+    res.locals.currentUser = req.user; //user throughout the app available ho har jgah. user ko currentUser name de diya and ye value req.user se lega. Toh ab aisa karenge ki...current user login hai toh login mat dikhao...and if current user login nai hai to dikhao.
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+})
+
+// Routes
+app.use(productRoutes);
+app.use(reviewRoutes);
+app.use(authRoutes); //upar auth routes bnaya and yha use kar rahe hai. 
+
+const port = 8080;
+app.listen(port,()=>{
+    console.log(`server connected at port : ${port}`);
+})
---
+++ Lecture-62/Ecommerce-v6/routes/auth.js
@@ -0,0 +1,46 @@
+const express =  require('express');
+const User = require('../models/User');
+const passport = require('passport');
+const router = express.Router();
+
+
+router.get('/register' , (req,res)=>{ //register kar rahe user ko 
+    res.render('auth/signup'); //response pe render kar diya signup ka page...auth ke andar ja and signup wala page uthalo..views m auth m bnaya signup.ejs. 
+})
+
+//POST request se data...request ki body m se milta hai.
+router.post('/register' , async (req,res)=>{ //jab signup pe click karega toh DB m aa jana chahiye...so DB m changes hote hai to POST request and /register path pe hi kara rahe hai.
+    // console.log(req.body);
+    let { username , password , email ,role, gender} = req.body; //npm passport local mongoose m...static method m register naam ka method. Yha destructure kar liya...request ki body m se jo ayega. new field add kari hai...toh yha role ayega.
+    // res.send('hi')
+    let user = new User({ username , email , gender, role}); //object. yha role bhejna padega
+    let newUser = await User.register(user , password);//register is a static method which means ye hamare model pe seedha lag jayega. register 2 chize accept kar rha...1st is user and 2nd is password. And since it's a DB operation...it'll take timea amd return a promise...so async and await. Ye register apne aap hi sare kaam karega...DB m store, username ko unique and password ko hash etc.
+    // res.send(newUser);
+    res.redirect('/login')//
+})
+
+router.get('/login' , (req,res)=>{ //login ka route
+    res.render('auth/login');
+})
+
+//login DB m jayega..DB m chizo ko check akrega...password ko match karega...
+router.post('/login', //path sirf login hai..isi pe hit karaya hai
+  passport.authenticate('local', //local strategy ke sath jana  hai. Passport ka use kar rahe hai toh ise reaquire karna padega.
+  { 
+    failureRedirect: '/login', //authentication fail ho gya toh firse login pe jana hai
+    failureMessage: true 
+  }),
+  function(req, res) { //else ye function run kar denge
+    // console.log(req.user , "User"); //jab bhi login hote hai toh...toh jo request hai usme automatically 1 object hota hai req.user. Check kar lete hai console kar ke..and string m User print karwa diya so that dikkat na ho. Now we want ki req.user hai iska access throughout the app available ho har jgah..toh jaise app.js m succes and error ko store kiya tha waise hi user ko store kar sakte hai...
+    req.flash('success' , `Welcome Back ${req.user.username}`)
+    res.redirect('/products');
+});
+
+router.get('/logout' , (req,res)=>{
+    req.logout(()=>{ //call back function bnaya jo automatically call ho rha hoga
+        req.flash('success' , 'Logged out successfully')
+        res.redirect('/login');
+    });
+})
+
+module.exports = router;
