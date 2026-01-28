369360ce04ee73069a10c8d7575a0ea4bb89f1ef
ashish29-tech <ashishranjan503@gmail.com>
2026-01-28 16:00:43 +0530

    Ecommerce v8

---
+++ Lecture-64/Ecommerce-v8/middleware.js
@@ -0,0 +1,73 @@
+const Product = require("./models/product");
+const { productSchema } = require("./schema");
+const { reviewSchema } = require("./schema");
+
+const validateProduct = (req,res,next)=>{
+    const {name, img, price , desc} = req.body;
+    const {error} = productSchema.validate({name,img,price,desc});
+    
+    if(error){
+        const msg = error.details.map((err)=>err.message).join(',');
+        return res.render('error' , {err:msg});
+    }
+    next();
+}
+
+const validateReview = (req,res,next)=>{
+
+    const {rating, comment} = req.body;
+    const {error} = reviewSchema.validate({rating,comment});
+
+    if(error){
+        const msg = error.details.map((err)=>err.message).join(',');
+        return res.render('error' , {err:msg});
+    }
+    next();
+}
+
+//new middleware bna dete hai...
+//- Jab user login na ho toh wo products dekh sake sirf...comment ya details wgr na dekh sake...iske liye login naam ka middleware bna sakte hai cuz ye route hit karne se pehle check karna hai..toh middleware toh for sure banega.....isLoggedin naam de dete hai. isLoggedin hoga toh hi ye next() next middleware pe ja payega wrna return ho jayega. Middleware ke liye page bna hi rakha hai.
+const isLoggedIn = (req,res,next)=>{
+
+         if(req.xhr && !req.isAuthenticated()){
+            // return res.error({msg : 'you need to login first'}) //error dikha denge or something like this...
+            return res.status(401).send('unauthorised');
+         //jaise hi api hit hoga toh isLoggedIn check hoga..
+        // console.log(req.xhr); //console kar ke dekhte hai kya hamare paas req.xhr hai ya nai...
+        }
+   
+    if(!req.isAuthenticated()){ ////req.isAuthenticated ye true or false return karta hai...aise hi check apne middleware m lga sakte hai. If false hai..means user login nai hai..
+        req.flash('error', 'You need to login first'); //error show kara do and message dikha do...You need...
+        return res.redirect('/login') //redirect /login page par
+    } 
+    next(); //if statement m nai jata mtlb login hota hai toh next middleware chala do. 
+}
+
+
+const isSeller = (req,res,next)=>{ //if seller nai hai toh wo na add, delete, edit kar payega...toh ek middleware..seller ka bnana padega..
+    let {id} = req.params; //destructure
+    if(!req.user.role){ //seller ke liye....user ka role hona jaruri hai..now role kha milega ? request hai toh req.user ka bhi access hoga and us user ke andar role naam ki chiz toh hogi hi...pehle check kar lete hai role hi nai hua toh
+        req.flash('error', 'You do not have the permission'); 
+        return res.redirect('/products')
+    }
+    else if(req.user.role !== "seller"){ // seller ke barabar nai hua toh
+        req.flash('error', 'You do not have the permission'); 
+        return res.redirect(`/products/${id}`) //id yha slash m lga di destructure kar ke
+    }
+    next();//agr sab shi hai toh
+}
+
+const isProductAuthor = async (req,res,next)=>{ //if seller hai..toh kya wo us product ka author hai..author nai hai toh edit, delete nai kar sakta...toh middleware bnayenge kya wo product ka author hai ya nai hai
+    let {id} = req.params;//product ka author hai ya nai iske liye sabse pehle product ki id chaiye hogi
+    let product = await Product.findById(id) //product id mil gyi toh product mil sakta hai..findById se pura product mil jayega...and since it's a DB command to it'll return us a promise...and promise works with async and await
+    console.log(product.author, 'author');// jo bande ne product bnaya hai. This is an object id.
+    console.log(req.user , 'user'); //jabnda logged in hai. This is also an object id.
+    if(!product.author.equals(req.user._id)){ //product author...req.user._id ke equal nai hua toh. 2 object id(product.author, req.user._id) ko cpmapre directly nai kar sakte...uske liye .equals method.
+        req.flash('error', 'You are not the owner of this product'); 
+        return res.redirect(`/products/${id}`) 
+    }
+    next();
+}
+
+
+module.exports = {validateProduct ,validateReview, isLoggedIn, isSeller, isProductAuthor} ; //ye teeno export kar diye yha se...and require kar diya productRoute.js m
---
+++ Lecture-64/Ecommerce-v8/routes/cart.js
@@ -0,0 +1,46 @@
+
+const express = require('express');
+const { isLoggedIn } = require('../middleware');
+const User = require('../models/User');
+const Product = require("../models/product");
+const router = express.Router();
+
+
+router.get('/user/cart', isLoggedIn, async (req,res)=>{ //yha bhi banda loggedIn chahiye isliye middleware lga ke check kara liya...
+  let userId = req.user._id; //
+  let user = await User.findById(userId).populate("cart"); //user bhi find kar lete hai...and logged in hai to user mil jayega
+  // console.log(user, "ashish");
+  //yha total amount calculate kar ke bhej sakte hai
+  let totalAmount = user.cart.reduce( (sum, curr) => sum + curr.price, 0) //this reduce method accepts a callback function..1 hi hai toh ham {}ye ignore karenge. curr is the item. user.cart m pura product hai.
+  // console.log(totalAmount);
+   
+  //cart ke andar cart.ejs hai...jisme sath m user ki sari information lelo
+  res.render("cart/cart", { user, totalAmount }); //jis pagae pe render kar rahe us cart wale page pe user ko bhi bhej dunga...page bna lenge....views ke andar cart and isme cart.ejs
+  ///user/:productId/add ye route tab hit karana hai jab koi add to cart pe click kare 
+  ///user/cart ye wale route jab navbar m bhi bnda cart pe click kar de...mtlb agr kisi ko apni cart check karni hai to wo click kar ke dekh sake...so partials -> navbar. Ye wala route tab hit hoga...jab ham add to cart button pe click kare...par kya ham button se koi post request bhej sakte hai ? No..toh hame ise form m convert karna padega...product m shows.ejs m 
+})
+
+//agr koi banda cart naam ke array m add kar pa raha hai that means wo bnda logged in ho. Middleware lagega.
+router.post('/user/:productId/add' , isLoggedIn, async (req,res)=> { //isLoggedIn ko use kar lenge and upar require kar lenge
+  let { productId } = req.params //product ki id chahiye hogi isliye req.params se kar lete hai...
+  let userId = req.user._id; //agr banda loggedIn hai toh req.user ka access toh hoga...id ko isliye find kiya so that user ko find kar pau and pure user ke andar jo cart naam ka array hoga...uske andar productId ko fek sakte hai
+  let user = await User.findById(userId); //user find karna hai..use kar rahe hai toh...User pe click kar ke upar require kar lenge and since itls a momgoDB method...it'll give me a promise...and promise works with async. populate lagayenge taki cart ke array se..sari chize utha ke user ko de denge
+  // console.log(user, "ashish");
+  let product = await Product.findById(productId); //product ko bhi find kar lete hai. 
+  user.cart.push(product); //user ki cart m push kar rahe hai pura ka pura product jo ultimately..productId hi bhejega 
+  await user.save(); //user ko save kara lete hai...and since it's a DB method..
+  //ye upar ke sare kaam ho jaye to user ko new page pe leke jana chahte hai..jha use sara cart dikhe
+  res.redirect('/user/cart'); // /user/cart pe GET request jayegi...
+
+});
+
+
+
+
+
+
+
+module.exports = router;
+
+
+
