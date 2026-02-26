591f3f9eb24ec5f968448025e44d6f2d11d84e0e
ashish29-tech <ashishranjan503@gmail.com>
2026-02-26 16:47:34 +0530

    chat app

---
+++ Lecture-76/chat-app/index.js
@@ -0,0 +1,61 @@
+//ye recive karega msg ko..server end pe
+
+const express = require('express')
+const app = express()
+const http = require("http"); //Require kar liya. http node ka part hai..isliye ise install ki jarurat nahi hai just like path ki trah...use bhi install nahi karna padta.
+//ek server bnaya http ka...
+const server = http.createServer(app) //nodejs m check karenge...1st chiz optional hai...2nd chiz requestListener. Toh app jo hai wo request handle kar leta hai.
+const path = require('path')
+const socketio = require('socket.io');
+const io = socketio(server); //socketio ke andar wrap kar diya...
+
+let users = {} //empty object bnaya
+
+
+//sabe pehle wrapper bnana hai...
+
+
+//jab bhi koi incoming request ayegi and hamne explicitly specify nahi kiya hai...toh public ke andar index.html file hogi wo...render ho jayegi.
+app.use('/', express.static(path.join(__dirname, 'public'))); //app.use toh middleware hai.../ means sari request pe chalna chahiye...express.static se...public se path connect kiya..
+
+
+io.on('connection', (socket) =>{ //io event accespt karta hai. it accepts a cb fn...jab connection ho jaye toh console kara do
+  console.log(`connection established with ${socket.id}`);
+
+//kuch data aya hoga frontend se...use catch kar liya
+socket.on("send-msg", (data) =>{ //it's an event based communication. Jo client pe bna rakha hai usi event ko use karte hai. cb fn.
+  // console.log(data); //console kara ke dekh lete hai data ko...
+  //received-msg lga ke ye msg frontend ko bhej diya hai...script.js m listen karna padega.
+  // socket.emit('received-msg', { //event based communication hai toh event banega received ka. msgs object ki form m jate hai
+  //   msg: data.msg,  //data ke andar se msg ko extract kiya
+  //   id: socket.id  //har socket ki unique id hoti hai...wo bhi bhej dete hai
+  // }) 
+
+  //io se emit karayenge chize...cuz same pipeline m rehna jaruri hai so that client 1, client 2...sab ek dusre ke msg dekh sake.
+  io.emit('received-msg', {
+    msg: data.msg,
+    // id: socket.id
+    username : users[socket.id] //is id ke corresponding m jo bhi naam pada ho...jo hamne login hone pe save karaya hai.
+  })
+}) 
+
+  socket.on('login', (data) =>{ //jab login ho jaye...login wale event ko frontend pe handle karna hai. cb fn chale usme data catch hoga...jo bhi us user ne bheja hoga
+    users [socket.id] = data.username; //us data ko set kar do users naam ke object m. socket.id naam ki field bnao usme set kar do..data.username...
+    // console.log(`User logged in: ${data.username}`);
+
+  })
+
+})
+
+
+
+
+
+
+// app.listen(3000, ()=>{
+//   console.log("Server connected to port")
+// })
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log("Server connected to port");
+});
---
+++ Lecture-76/chat-app/public/script.js
@@ -0,0 +1,55 @@
+//ye script.js frontend ko control karegi...client se msg bhej rahi hai
+//backend ko control index.js karegi
+//client frontend hai
+//server backend hai
+
+
+
+const socket = io()
+
+//chatbox ko id di hai index.html m
+$("#chat-box").hide();
+
+//jQuery m documnet.querySelector ki jgah ye likhte hai.
+$("#send-btn").on('click', ()=>{ //id ke basis pe is send button ko select karna hai...uske upar event lagaya...jab ispe click ho toh ek cb fn run hoga...
+   const msgTxt = $("#inp").val();  //msgTxt ko index.html m jo inp id de rakhi hai wha se nikal lenge....yani ki text area ko. document.querySelector ki jgah..$()...  .val() isme function/method hote hai isliye parenthesis
+
+
+   //pehli baar message aya toh...event..bna tha send-msg ka...and is msg ko backend pe listen kara tha
+   socket.emit('send-msg', { // socket jo data ko send karta hai uske liye ek keyword use karta hai...isse ye keywords ko bhejta hai..and socket accepts a event...
+      msg: msgTxt  //msg jo bhejte hai wo object ki form m hota hai and obj m key:value pair hota hai.
+   }) 
+
+   $("#inp").val("") //bhejne ke baad khali hona chahiye textbox...toh jo input tha uski val ko kar do khali...
+
+}) 
+
+socket.on('received-msg', (data) =>{ //received-msg ko listen karte hai. cb fn hoga. data ko catch kar liya.
+  // console.log(data)
+  //yha dom manipulation kiya.
+  $("#chat").append(`<li><span>${data.id}</span> -> ${data.msg}</li>`) //index.html m ul ko id di chat and yha chat ko catch kiya and append kara liya...ul ke andar li hi hote hai...data m se id and msg ko extract kar liya.
+})
+
+
+//button ko id de di index.html m and yha uspe event lagate hai
+$("#login-btn").on('click', ()=>{ //login button pe click karne par...1 cb fn chal jaye.
+  const username = $("#username").val() //id dedi user name ki index.html m. .val se value aa jayegi.
+  
+
+  socket.emit('login', { //socket ek message bhejne wala hai..ek event bna ke bhejega ki jab login ho jaye toh 1 obj bhej do...jiske andar username pada ho.
+    username : username
+  })
+
+  //login karne baad..login ko hide kar do
+  $("#login").hide()
+  $("#chat-box").show()
+  
+
+
+  //jaise hi login button press ho toh pehle se given username ko khali kar dena hai
+  $("#username").val("") //khali kar do
+}) 
+
+
+
+
