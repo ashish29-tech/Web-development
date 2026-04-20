b1c7980d78e0d6f167a3fa1df2070268be476ce2
ashish29-tech <ashishranjan503@gmail.com>
2026-04-20 23:47:06 +0530

    update

---
+++ Lecture-22/execution-context/app.js
@@ -0,0 +1,46 @@
+//Empty JS file is the shortest pragramme which we can write in js....matlab bilkul khali file
+
+// var userName = "Ashish";
+
+// var person = {
+//   email : "ashish@gmail.com",
+//   password : "ashish",
+// }
+
+// console.log(userName);
+// console.log(person);
+
+
+//--------------------------------------
+
+// var userName = "devi prasad";
+
+// function sum(){ //function ka kaam hota hai to return something
+//   var num1 = 10;
+//   var num2 = 20;
+
+//   return num1 + num2;
+// }
+
+// sum(); //call sum function. But kahi store nai kiya to 30 nai ayega
+// console.log(sum());
+// console.log(userName);
+
+//-------------------------------
+
+// var naam = "ashish"; 
+
+// function ash(){
+//   console.log("inside ash");
+//   var a = 10;
+//   console.log(a);
+// }
+
+// ash();
+// console.log(naam);
+  
+
+
+
+
+
---
+++ Lecture-22/most-asked-questions/app.js
@@ -0,0 +1,47 @@
+
+var a = 20;
+
+function meraFunction(){
+  console.log("inside mera function");
+}
+
+console.log(a);
+meraFunction();
+
+//----------------------------------------
+//--------HOISTING-----------=
+//Whenever we access the variable or the function before its declaration 
+// then it is called hoisting
+
+// console.log(a); //use pehle hua
+// meraFunction(); //use pehle hua
+
+// var a = 20; //define baad m hua
+
+// function meraFunction(){ //define baad m hua
+//   console.log("inside mera function");
+// }
+
+//---------------------------------------------
+// Is there hoisting ? 
+// Yes HOISTING ho rahi hai
+
+// function a() { //define
+//   console.log(b); //use
+//   var b = 100 //define
+// }
+
+// a(); //access
+
+
+
+
+
+
+
+
+
+
+
+
+
